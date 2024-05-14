f77c5e9feb31ab6bd43954d3c4cb3da535b424eb
Manuel <manuelloverso60@gmail.com>
2024-05-14 17:33:43 +0200

    add data on page

--- resources/views/guests/home.blade.php
+++ resources/views/guests/home.blade.php
@@ -1,8 +1,20 @@
 @extends('layouts.app')
 
 @section('content')
-
-<h1>Home</h1>
-
-
+    <div class="container">
+        <div class="row">
+            @foreach ($items as $item)
+                <div class="col-4">
+                    <div class="card">
+                        <div class="card-body">
+                            <h1>{{ $item->name }}</h1>
+                            <h5>{{ $item->slug }}</h5>
+                            <h5>{{ $item->type }}</h5>
+                            <h5>{{ $item->category }}</h5>
+                        </div>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 @endsection
