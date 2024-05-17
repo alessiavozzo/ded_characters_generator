b8f739a8f3f1e1cfefcb70270b62a12ba0d820ef
alessiavozzo <a.vozzo95@gmail.com>
2024-05-17 16:14:09 +0200

    add characters seeder using faker close #3

--- database/seeders/CharacterSeeder.php
+++ database/seeders/CharacterSeeder.php
@@ -2,16 +2,34 @@
 
 namespace Database\Seeders;
 
+use App\Models\Character;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
 
 class CharacterSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run(Faker $faker): void
     {
-        //
+        /* $table->id();
+            $table->string('name',50);
+            $table->text('description');
+            $table->smallInteger('attack');
+            $table->smallInteger('defense');
+            $table->string('speed',50);
+            $table->timestamps(); */
+
+        for ($i=0; $i < 10; $i++) { 
+            $character = new Character();
+            $character->name = $faker->name();
+            $character->description = $faker->text();
+            $character->attack = $faker->numberBetween(1, 100);
+            $character->defense = $faker->numberBetween(1, 100);
+            $character->speed = $faker->numerify('##m');
+            $character->save();
+        }
     }
 }
