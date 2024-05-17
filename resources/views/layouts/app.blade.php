d24203f16b70b6c65c87187b08199aa0221abb64
marinodilauro <m.dilauro90@gmail.com>
2024-05-17 15:43:50 +0200

    Add character model with controller, seeder, migration and form request close #1

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
-<head>
+  <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel @yield('page-title','Alessia-page')</title>
+    <title>Laravel @yield('page-title', 'D&D Character generator')</title>
 
     <!-- Fonts -->
     <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
@@ -13,19 +13,19 @@
     <!-- Styles -->
     @vite('resources/js/app.js')
 
-</head>
+  </head>
 
-<body>
+  <body>
     @include('partials.header')
 
     <main>
-        @yield('content')
+      @yield('content')
     </main>
 
     @include('partials.footer')
 
 
 
-</body>
+  </body>
 
 </html>
