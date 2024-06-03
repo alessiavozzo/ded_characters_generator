2336263ac48283dace6f1ef244debeb20f78d471
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-06-03 16:28:38 +0200

    add show crud for items close #49

--- app/Http/Controllers/Admin/TypeController.php
+++ app/Http/Controllers/Admin/TypeController.php
@@ -1,10 +1,11 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
 
 use App\Models\Type;
 use App\Http\Requests\StoreTypeRequest;
 use App\Http\Requests\UpdateTypeRequest;
+use App\Http\Controllers\Controller;
 
 class TypeController extends Controller
 {
@@ -38,7 +39,7 @@ class TypeController extends Controller
      */
     public function show(Type $type)
     {
-        //
+        return view('admin.types.show', compact('type'));
     }
 
     /**
